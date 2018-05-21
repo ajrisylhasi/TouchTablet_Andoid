662daacb98398966b71dbad3a2b21497d2dcdb40
Ajri Sylhasi <ajrisylhasi@hotmail.com>
2018-05-21 18:25:50 +0200

    Working pretty well, stable at 250 milis.

--- app/build/generated/source/r/debug/android/support/fragment/R.java
+++ app/build/generated/source/r/debug/android/support/fragment/R.java
@@ -9,32 +9,32 @@ package android.support.fragment;
 public final class R {
     public static final class attr {
         public static final int coordinatorLayoutStyle = 0x7f020064;
-        public static final int font = 0x7f020078;
-        public static final int fontProviderAuthority = 0x7f02007a;
-        public static final int fontProviderCerts = 0x7f02007b;
-        public static final int fontProviderFetchStrategy = 0x7f02007c;
-        public static final int fontProviderFetchTimeout = 0x7f02007d;
-        public static final int fontProviderPackage = 0x7f02007e;
-        public static final int fontProviderQuery = 0x7f02007f;
-        public static final int fontStyle = 0x7f020080;
-        public static final int fontWeight = 0x7f020081;
-        public static final int keylines = 0x7f020091;
-        public static final int layout_anchor = 0x7f020093;
-        public static final int layout_anchorGravity = 0x7f020094;
-        public static final int layout_behavior = 0x7f020095;
-        public static final int layout_dodgeInsetEdges = 0x7f0200bf;
-        public static final int layout_insetEdge = 0x7f0200c8;
-        public static final int layout_keyline = 0x7f0200c9;
-        public static final int statusBarBackground = 0x7f020104;
+        public static final int font = 0x7f020079;
+        public static final int fontProviderAuthority = 0x7f02007b;
+        public static final int fontProviderCerts = 0x7f02007c;
+        public static final int fontProviderFetchStrategy = 0x7f02007d;
+        public static final int fontProviderFetchTimeout = 0x7f02007e;
+        public static final int fontProviderPackage = 0x7f02007f;
+        public static final int fontProviderQuery = 0x7f020080;
+        public static final int fontStyle = 0x7f020081;
+        public static final int fontWeight = 0x7f020082;
+        public static final int keylines = 0x7f020094;
+        public static final int layout_anchor = 0x7f020097;
+        public static final int layout_anchorGravity = 0x7f020098;
+        public static final int layout_behavior = 0x7f020099;
+        public static final int layout_dodgeInsetEdges = 0x7f0200c3;
+        public static final int layout_insetEdge = 0x7f0200cc;
+        public static final int layout_keyline = 0x7f0200cd;
+        public static final int statusBarBackground = 0x7f02010f;
     }
     public static final class bool {
         public static final int abc_action_bar_embed_tabs = 0x7f030000;
     }
     public static final class color {
-        public static final int notification_action_color_filter = 0x7f04003e;
-        public static final int notification_icon_bg_color = 0x7f04003f;
-        public static final int ripple_material_light = 0x7f04004b;
-        public static final int secondary_text_default_material_light = 0x7f04004d;
+        public static final int notification_action_color_filter = 0x7f04003f;
+        public static final int notification_icon_bg_color = 0x7f040040;
+        public static final int ripple_material_light = 0x7f04004d;
+        public static final int secondary_text_default_material_light = 0x7f04004f;
     }
     public static final class dimen {
         public static final int compat_button_inset_horizontal_material = 0x7f05004d;
@@ -73,39 +73,39 @@ public final class R {
         public static final int notify_panel_notification_icon_bg = 0x7f060062;
     }
     public static final class id {
-        public static final int action_container = 0x7f07000e;
-        public static final int action_divider = 0x7f070010;
-        public static final int action_image = 0x7f070011;
-        public static final int action_text = 0x7f070017;
-        public static final int actions = 0x7f070018;
-        public static final int async = 0x7f07001e;
-        public static final int blocking = 0x7f070022;
-        public static final int bottom = 0x7f070023;
-        public static final int chronometer = 0x7f07002f;
-        public static final int end = 0x7f07003c;
-        public static final int forever = 0x7f070042;
-        public static final int icon = 0x7f07004a;
-        public static final int icon_group = 0x7f07004b;
-        public static final int info = 0x7f07004e;
-        public static final int italic = 0x7f070051;
-        public static final int left = 0x7f070052;
-        public static final int line1 = 0x7f070053;
-        public static final int line3 = 0x7f070054;
-        public static final int none = 0x7f07005b;
-        public static final int normal = 0x7f07005c;
-        public static final int notification_background = 0x7f07005d;
-        public static final int notification_main_column = 0x7f07005e;
-        public static final int notification_main_column_container = 0x7f07005f;
-        public static final int right = 0x7f070068;
-        public static final int right_icon = 0x7f070069;
-        public static final int right_side = 0x7f07006a;
-        public static final int start = 0x7f070088;
-        public static final int tag_transition_group = 0x7f07008c;
-        public static final int text = 0x7f07008e;
-        public static final int text2 = 0x7f07008f;
-        public static final int time = 0x7f070092;
-        public static final int title = 0x7f070093;
-        public static final int top = 0x7f070096;
+        public static final int action_container = 0x7f07000d;
+        public static final int action_divider = 0x7f07000f;
+        public static final int action_image = 0x7f070010;
+        public static final int action_text = 0x7f070016;
+        public static final int actions = 0x7f070017;
+        public static final int async = 0x7f07001d;
+        public static final int blocking = 0x7f070021;
+        public static final int bottom = 0x7f070022;
+        public static final int chronometer = 0x7f07002c;
+        public static final int end = 0x7f07003a;
+        public static final int forever = 0x7f070040;
+        public static final int icon = 0x7f070048;
+        public static final int icon_group = 0x7f070049;
+        public static final int info = 0x7f07004c;
+        public static final int italic = 0x7f07004f;
+        public static final int left = 0x7f070050;
+        public static final int line1 = 0x7f070051;
+        public static final int line3 = 0x7f070052;
+        public static final int none = 0x7f070059;
+        public static final int normal = 0x7f07005a;
+        public static final int notification_background = 0x7f07005b;
+        public static final int notification_main_column = 0x7f07005c;
+        public static final int notification_main_column_container = 0x7f07005d;
+        public static final int right = 0x7f070066;
+        public static final int right_icon = 0x7f070067;
+        public static final int right_side = 0x7f070068;
+        public static final int start = 0x7f070084;
+        public static final int tag_transition_group = 0x7f070088;
+        public static final int text = 0x7f07008a;
+        public static final int text2 = 0x7f07008b;
+        public static final int time = 0x7f07008e;
+        public static final int title = 0x7f07008f;
+        public static final int top = 0x7f070092;
     }
     public static final class integer {
         public static final int status_bar_notification_info_maxnum = 0x7f080004;
@@ -132,10 +132,10 @@ public final class R {
         public static final int Widget_Support_CoordinatorLayout = 0x7f0c0155;
     }
     public static final class styleable {
-        public static final int[] CoordinatorLayout = { 0x7f020091, 0x7f020104 };
+        public static final int[] CoordinatorLayout = { 0x7f020094, 0x7f02010f };
         public static final int CoordinatorLayout_keylines = 0;
         public static final int CoordinatorLayout_statusBarBackground = 1;
-        public static final int[] CoordinatorLayout_Layout = { 0x010100b3, 0x7f020093, 0x7f020094, 0x7f020095, 0x7f0200bf, 0x7f0200c8, 0x7f0200c9 };
+        public static final int[] CoordinatorLayout_Layout = { 0x010100b3, 0x7f020097, 0x7f020098, 0x7f020099, 0x7f0200c3, 0x7f0200cc, 0x7f0200cd };
         public static final int CoordinatorLayout_Layout_android_layout_gravity = 0;
         public static final int CoordinatorLayout_Layout_layout_anchor = 1;
         public static final int CoordinatorLayout_Layout_layout_anchorGravity = 2;
@@ -143,14 +143,14 @@ public final class R {
         public static final int CoordinatorLayout_Layout_layout_dodgeInsetEdges = 4;
         public static final int CoordinatorLayout_Layout_layout_insetEdge = 5;
         public static final int CoordinatorLayout_Layout_layout_keyline = 6;
-        public static final int[] FontFamily = { 0x7f02007a, 0x7f02007b, 0x7f02007c, 0x7f02007d, 0x7f02007e, 0x7f02007f };
+        public static final int[] FontFamily = { 0x7f02007b, 0x7f02007c, 0x7f02007d, 0x7f02007e, 0x7f02007f, 0x7f020080 };
         public static final int FontFamily_fontProviderAuthority = 0;
         public static final int FontFamily_fontProviderCerts = 1;
         public static final int FontFamily_fontProviderFetchStrategy = 2;
         public static final int FontFamily_fontProviderFetchTimeout = 3;
         public static final int FontFamily_fontProviderPackage = 4;
         public static final int FontFamily_fontProviderQuery = 5;
-        public static final int[] FontFamilyFont = { 0x01010532, 0x01010533, 0x0101053f, 0x7f020078, 0x7f020080, 0x7f020081 };
+        public static final int[] FontFamilyFont = { 0x01010532, 0x01010533, 0x0101053f, 0x7f020079, 0x7f020081, 0x7f020082 };
         public static final int FontFamilyFont_android_font = 0;
         public static final int FontFamilyFont_android_fontWeight = 1;
         public static final int FontFamilyFont_android_fontStyle = 2;
--- app/src/main/java/org/bonevet/ajri_communication/MainActivity.java
+++ app/src/main/java/org/bonevet/ajri_communication/MainActivity.java
@@ -14,9 +14,11 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.RequiresApi;
 import android.util.Log;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.felhr.usbserial.UsbSerialDevice;
 import com.felhr.usbserial.UsbSerialInterface;
@@ -27,10 +29,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import de.nitri.gauge.Gauge;
+
+@SuppressWarnings("ALL")
 public class MainActivity extends Activity {
     public final String ACTION_USB_PERMISSION = "org.bonevet.ajri_communication.USB_PERMISSION";
     Button startButton, stopButton, btRelays;
-    TextView vl_intensity, vl_voltage, vl_battery, vl_temp, vl_range, vl_speed, vl_rpm;
+    TextView vl_intensity, vl_voltage, vl_battery, vl_temp, vl_range;
     Integer status = 1;
     Integer i = 0;
     List<String> vlerat = new ArrayList<>();
@@ -40,6 +45,9 @@ public class MainActivity extends Activity {
     UsbDevice device;
     UsbSerialDevice serialPort;
     UsbDeviceConnection connection;
+    int speed_value, rpm_value;
+    Gauge gauge, gauge2;
+    private int currentApiVersion;
 
     UsbSerialInterface.UsbReadCallback mCallback = new UsbSerialInterface.UsbReadCallback() { //Defining a Callback which triggers whenever data is read.
         @RequiresApi(api = Build.VERSION_CODES.O)
@@ -49,32 +57,34 @@ public class MainActivity extends Activity {
                 data = new String(arg0, "UTF-8");
                 vlerat.add(data);
                 StringBuilder sb = new StringBuilder();
-                for (String s : vlerat)
-                {
+                for (String s : vlerat) {
                     sb.append(s);
                 }
                 stringi = sb.toString();
-                if(data.contains("!")){
+                if (data.contains("!")) {
                     te_dhenat = stringi.split("-");
                     try {
-                        Thread.sleep(200);
+                        Thread.sleep(0);
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                vl_intensity.setText(te_dhenat[0]+" A");
-                                vl_voltage.setText(te_dhenat[1]+" V");
-                                vl_range.setText(te_dhenat[4]+" km");
-                                vl_battery.setText(te_dhenat[2]+" %");
-                                vl_speed.setText(te_dhenat[5]+" km/h");
-                                vl_temp.setText(te_dhenat[3]+" °C");
-                                vl_rpm.setText(te_dhenat[6]+" rpms");
+                                vl_intensity.setText(te_dhenat[0] + " A");
+                                vl_voltage.setText(te_dhenat[1] + " V");
+                                vl_battery.setText(te_dhenat[2] + " %");
+                                vl_temp.setText(te_dhenat[3] + " °C");
+                                vl_range.setText(te_dhenat[4] + " km");
+                                speed_value= Integer.parseInt(te_dhenat[5]);
+                                rpm_value= Integer.parseInt(te_dhenat[6]);
+                                gauge.moveToValue(speed_value);
+                                gauge2.moveToValue(rpm_value);
                             }
                         });
-                    } catch (InterruptedException e){
+                    } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                     vlerat.clear();
                 }
+
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
@@ -84,7 +94,7 @@ public class MainActivity extends Activity {
     private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() { //Broadcast Receiver to automatically start and stop the Serial connection.
         @Override
         public void onReceive(Context context, Intent intent) {
-            if (intent.getAction().equals(ACTION_USB_PERMISSION)) {
+            if (intent.getAction()==(ACTION_USB_PERMISSION)) {
                 boolean granted = intent.getExtras().getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED);
                 if (granted) {
                     connection = usbManager.openDevice(device);
@@ -104,6 +114,7 @@ public class MainActivity extends Activity {
                                     btRelays.setClickable(true);
                                 }
                             }, 1700);
+                            Toast.makeText(context, "Device Open", Toast.LENGTH_SHORT).show();
                         } else {
                             Log.d("SERIAL", "PORT NOT OPEN");
                         }
@@ -120,7 +131,7 @@ public class MainActivity extends Activity {
                     for (Map.Entry<String, UsbDevice> entry : usbDevices.entrySet()) {
                         device = entry.getValue();
                         int deviceVID = device.getVendorId();
-                        if (deviceVID == 0x2341)//Arduino Vendor ID
+                        if (deviceVID == 0x2341 || deviceVID == 0x2A03)
                         {
                             PendingIntent pi = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_PERMISSION), 0);
                             usbManager.requestPermission(device, pi);
@@ -129,13 +140,21 @@ public class MainActivity extends Activity {
                             connection = null;
                             device = null;
                         }
-
                         if (!keep)
                             break;
                     }
                 }
             } else if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_DETACHED)) {
-                onClickStop(stopButton);
+                if (serialPort == null) {
+
+                } else {
+                    onClickStop(stopButton);
+                    Toast.makeText(context, "Device Closed", Toast.LENGTH_SHORT).show();
+                    btRelays.setText("Ndalur");
+                    btRelays.setBackgroundResource(R.drawable.round1);
+                    serialPort.write("0".getBytes());
+                    status = 1;
+                }
             }
         }
 
@@ -150,13 +169,42 @@ public class MainActivity extends Activity {
         btRelays = findViewById(R.id.btRelays);
         stopButton = findViewById(R.id.buttonClose);
         vl_intensity = findViewById(R.id.vl_intensity);
-        vl_voltage =  findViewById(R.id.vl_voltage);
+        vl_voltage = findViewById(R.id.vl_voltage);
         vl_battery = findViewById(R.id.vl_battery);
         vl_temp = findViewById(R.id.vl_temp);
         vl_range = findViewById(R.id.vl_range);
-        vl_speed = findViewById(R.id.vl_speed);
-        vl_rpm = findViewById(R.id.vl_rpm);
+
         setUiEnabled(false);
+
+        currentApiVersion = android.os.Build.VERSION.SDK_INT;
+
+        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        if(currentApiVersion >= Build.VERSION_CODES.KITKAT)
+        {
+            getWindow().getDecorView().setSystemUiVisibility(flags);
+            final View decorView = getWindow().getDecorView();
+            decorView
+                    .setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener()
+                    {
+                        @Override
+                        public void onSystemUiVisibilityChange(int visibility)
+                        {
+                            if((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0)
+                            {
+                                decorView.setSystemUiVisibility(flags);
+                            }
+                        }
+                    });
+        }
+
+        gauge = findViewById(R.id.gauge);
+        gauge2 = findViewById(R.id.gauge2);
+
 
         IntentFilter filter = new IntentFilter();
         filter.addAction(ACTION_USB_PERMISSION);
@@ -166,6 +214,22 @@ public class MainActivity extends Activity {
 
         btRelays.setBackgroundResource(R.drawable.round1);
 
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus)
+    {
+        super.onWindowFocusChanged(hasFocus);
+        if(currentApiVersion >= Build.VERSION_CODES.KITKAT && hasFocus)
+        {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 
     public void onClickSwitch(View v) {
@@ -214,7 +278,6 @@ public class MainActivity extends Activity {
         startButton.setEnabled(!bool);
         stopButton.setEnabled(bool);
         btRelays.setEnabled(bool);
-
     }
 
     public void onClickStart(View view) {
@@ -225,7 +288,7 @@ public class MainActivity extends Activity {
             for (Map.Entry<String, UsbDevice> entry : usbDevices.entrySet()) {
                 device = entry.getValue();
                 int deviceVID = device.getVendorId();
-                if (deviceVID == 0x2341)//Arduino Vendor ID
+                if (deviceVID == 0x2341 || deviceVID == 0x2A03)
                 {
                     PendingIntent pi = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
                     usbManager.requestPermission(device, pi);
@@ -234,30 +297,19 @@ public class MainActivity extends Activity {
                     connection = null;
                     device = null;
                 }
-
                 if (!keep)
                     break;
             }
         }
-
-
     }
 
     public void onClickStop(View view) {
         setUiEnabled(false);
         serialPort.close();
-    }
-
-    private void tvAppend(TextView tv, CharSequence text) {
-        final TextView ftv = tv;
-        final CharSequence ftext = text;
-
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                ftv.append(ftext);
-            }
-        });
+        btRelays.setText("Ndalur");
+        btRelays.setBackgroundResource(R.drawable.round1);
+        serialPort.write("0".getBytes());
+        status = 1;
     }
 
 }
